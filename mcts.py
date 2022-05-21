5805a68bcd5c9cb65e7b2149bec962f1857235d5
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-05-20 21:09:49 -0700

    Placeholder for mcts, design incoming after reading board code

---
+++ mcts.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# AI 531 - m,n,k (MCTS)
+# Wadood Alam
+# Joe Nguyen
+# Matthew Pacey
+
+class MCTS:
+    def __init__(self, play1, play2):
+        '''
+        Init Monte Carlo Tree Search with two players
+        :param play1:
+        :param play2:
+        '''
+        pass
+
