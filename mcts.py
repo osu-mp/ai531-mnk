a246c96ed17cf65c75ff6ddc8971c9313697a689
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-05-24 23:07:41 -0700

    improved mcts working, committing to branch prior to cleanup

--- mcts.py
+++ mcts.py
@@ -18,9 +18,137 @@ branch_factor = 10
 max_trials = 1000
 uct_const = math.sqrt(2)            # constant term in uct evaluation
 
-# def mcts_get_best_move(board: Board, player1, player2):
+max_mcts_loops = 2000      # TODO: this should obviously be higher, low for debug
 
+DEBUG = False
+
+def log(msg: str):
+    # print log messages to console if debugging enabled
+    if DEBUG:
+        print(msg)
+
+class Node:
+    def __init__(self, board, player, parent=None, square=None):
+        self.board = deepcopy(board)
+        self.parent = parent
+        self.square = square
+        self.children = []
+        self.wins = 0
+        self.games = 0
+        self.player = player
+
+    def get_uct(self):
+        if self.games == 0:         # avoid divide by zero
+            return 0
+
+        if not self.parent:         # return raw win percentage for root node
+            return self.wins / self.games
+
+        # else uct = wins / games + C * sqrt(log n * Parent(n) / n)
+        n = self.games  # number of games simulated at this level
+        value = self.wins / n  # exploitation
+        value += uct_const * math.sqrt(math.log(n) * self.parent.get_uct() / n)
+
+        return value
+
+    def expand(self):
+        squares = self.board.get_empty_squares()
+        for square in squares:
+            node = Node(self.board, self, square)
+            self.children.append(node)
+
+
+def mcts_new(board: Board, player):
+    # TODO add time tracker?
+    loops = 0
+    root = Node(board, player, None, None)
+    for square in board.get_empty_squares():
+        node = Node(board, player, root, square)
+        root.children.append(node)
+
+    log('\nNEW MCTS RUN')
+
+    while loops < max_mcts_loops:
+        loops += 1
+        # selection
+        node = select_node(root)
+        # print(f'Selected node at square: {node.square}')
+        # expansion
+
+        # simulation
+        result = playout(node)
+        log(f'Result of playout ({node.square}): {result}')
+
+        # backpropagation
+        backpropagate(node, result)
+
+    best_node = select_node(root)
+    log(f'Best node of current root: {best_node.square}')
+    return best_node.square
+
+def select_node(node: Node):
+
+    # if any nodes are unvisited, try them first
+    for child in node.children:
+        if child.games == 0:
+            log(f'Returning unvisited node: {child.square}')
+            return child
+
+    while len(node.children) > 0:
+        best_uct = -1
+        # pick the child node with the highest uct
+        for child in node.children:
+            uct = child.get_uct()
+            log(f'UCT of {child.square} = {uct}')
+            if uct > best_uct:
+                best_uct = uct
+                node = child
+
+    return node
+
+def playout(node):
+    # create local copy of board to simulate playout
+    board = Board(size=node.board.size, k=node.board.k, board=node.board.board)
+
+    empty_squares = board.get_empty_squares()
+    curr_player = get_other_player(node.player)  # player2 should make first move since player1 selected square prior to trial
+    while empty_squares:
+        selected_square = empty_squares[random.randrange(len(empty_squares))]
+        board.make_move(selected_square, curr_player)
+        empty_squares = board.get_empty_squares()
+        curr_player = get_other_player(curr_player)  # alternate between player 1 and 2 each loop
+
+        if board.is_gameover(selected_square, node.player):
+            if board.is_win(selected_square, node.player):
+                return 1
+            return 0
+
+    if board.is_gameover(selected_square, node.player):
+        if board.is_win(selected_square, node.player):
+            return 1
+    return 0
+
+def backpropagate(node: Node, result: int):
+    node.games += 1
+    if result:
+        node.wins += 1
+    if not node.parent:
+        return
+    backpropagate(node.parent, result)
+
+def best_move(root: Node):
+    pass
+
+# def mcts_get_best_move(board: Board, player1, player2):
 def mcts(board: Board, player1, policy=None):
+    """
+    Simulate nodes from given board (until end of game)
+    Return best action
+    :param board:
+    :param player1:
+    :param policy:
+    :return:
+    """
     # for each possible square in the board, run a simulation
     # selection
     empty_squares = board.get_empty_squares()
@@ -182,6 +310,7 @@ def policy_random(board, player):
     selected = empty_squares[random.randrange(len(empty_squares))]
     return selected
 
+
 class TestMCTS(unittest.TestCase):
     def setUp(self) -> None:
         self.board = Board((3, 3), 3)
@@ -225,6 +354,27 @@ class TestMCTS(unittest.TestCase):
             player = get_other_player(player)
 
         print(f'Winning player: {self.board.winner}')
+
+    def test_mcts_new(self):
+        """
+        Work in progress test for mcts
+        :return:
+        """
+        # mcts = MCTS(self.board, 1)
+        player = 1
+        while len(self.board.get_empty_squares()) > 0:
+            best_move = mcts_new(self.board, player)
+            self.board.make_move(best_move, player)
+            if self.board.is_win(best_move, player):
+                print(f'Player {player} wins!')
+                self.board.show()
+                break;
+            print(f'Best move for player {player} is: {best_move}')
+            self.board.show()
+            player = get_other_player(player)
+
+        print(f'Winning player: {self.board.winner}')
+        self.assertTrue(self.board.winner != 2, 'Player 2 should never win if strategy is correct')
 
 
 if __name__ == '__main__':
