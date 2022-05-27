aa4d109f7f6396417b08de08844448c11c44b7dc
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-05-26 22:01:59 -0700

    Added config file for easy access to shared constants

---
+++ cfg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# AI 531 - m,n,k
+# Wadood Alam
+# Joe Nguyen
+# Matthew Pacey
+
+uct_const = 1.25# math.sqrt(2)            # constant term in uct evaluation
+max_mcts_loops = 1000               # number of times to run mcts at each eval
+DEBUG = False                       # set to True for verbose debugging messages
