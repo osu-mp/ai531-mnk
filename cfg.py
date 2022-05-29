5cc65045980fc13440499fc6fbb17eab92ab2f52
osu-mp <97149606+osu-mp@users.noreply.github.com>
2022-05-29 09:24:41 -0700

    Latest data for mcts, still wip to get algo improved

--- cfg.py
+++ cfg.py
@@ -6,7 +6,7 @@
 # Matthew Pacey
 
 uct_const = 3 # math.sqrt(2)            # constant term in uct evaluation
-max_mcts_loops = 500               # number of times to run mcts at each eval
+max_mcts_loops = 250               # number of times to run mcts at each eval
 
 # dial for which node is expanded; if random number between 0 and 1 is greater than this, random node
 # will be generated; else best node will be expanded using policy
