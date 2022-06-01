bb9c913679b21e99b9a4bb58b0fc22a1a555b7b6
gofortargets <gofortargets@gmail.com>
2022-06-01 14:53:43 -0700

    speed up in alphaBeta.py: save first_player

--- alphaBeta.py
+++ alphaBeta.py
@@ -8,25 +8,26 @@ ab_time_filename = 'abTime.txt'
 
 
 # timeFile_for_ab = open("abTime.txt", 'w')
+#
+# # Player saved in the file from bot_Move
+# def save_player(player):
+#     f = open('cur_player.txt', 'w')
+#     f.write(str(player))
+#     f.close()
+#
+#
+# # Player retrieved from the file
+# def get_player():
+#     f = open('cur_player.txt', 'r')
+#     player = int(f.readline())
+#     f.close()
+#     return player
 
-# Player saved in the file from bot_Move
-def save_player(player):
-    f = open('cur_player.txt', 'w')
-    f.write(str(player))
-    f.close()
 
-
-# Player retrieved from the file
-def get_player():
-    f = open('cur_player.txt', 'r')
-    player = int(f.readline())
-    f.close()
-    return player
-
-
-def alphaBeta(position: Board, depth, alpha, beta, player, previousMove):
+def alphaBeta(position: Board, depth, alpha, beta, player, previousMove, first_player):
     if position.is_gameover(previousMove, get_other_player(player)) or depth == 0:
-        activePlayer = get_player()
+        # activePlayer = get_player()
+        activePlayer = first_player
         if activePlayer == 2:
             return position.winner * (-1)
         elif activePlayer == 1:
@@ -34,12 +35,13 @@ def alphaBeta(position: Board, depth, alpha, beta, player, previousMove):
         return 0
 
     for i in position.get_empty_squares():
-        positionCopy = deepcopy(position)
+        positionCopy = deepcopy(position)  # type: Board
         positionCopy.make_move(i, player)
         # recursive call with a deepcopy of the position, opposing player, and the updated move or i
-        val = alphaBeta(positionCopy, depth - 1, alpha, beta, get_other_player(player), i)
+        val = alphaBeta(positionCopy, depth - 1, alpha, beta, get_other_player(player), i, first_player)
         # If player is alpha
-        if player == get_player():
+        # if player == get_player():
+        if player == first_player:
             if alpha > val:
                 # update max alpha
                 alpha = val
@@ -50,24 +52,25 @@ def alphaBeta(position: Board, depth, alpha, beta, player, previousMove):
                 beta = val
             if beta <= alpha:
                 return alpha
-    if player == get_player():
+    # if player == get_player():
+    if player == first_player:
         return alpha
     else:
         return beta
 
 
-def ab_bot(position: Board, player):
+def ab_bot(position: Board, player: int):
     a = -2
     moveChoices = []
 
     # if len(position.get_empty_squares()) == position.size[][] ** 2: # best 1st move
     #    return position.size[1] ** 2 // 2 + 1
-
+    first_player = deepcopy(player)
     players = [0, 1, 2]
     for move in position.get_empty_squares():
         clone = deepcopy(position)
         clone.make_move(move, player)
-        val = alphaBeta(clone, 4, -2, 2, get_other_player(player), move)
+        val = alphaBeta(clone, 4, -2, 2, get_other_player(player), move, first_player)
         log(f"move {move} causes player {players[val]} to win")
         if val > a:
             a = val
@@ -78,13 +81,13 @@ def ab_bot(position: Board, player):
 
 
 def bot_move(position: Board, player, algoType):
-    save_player(player)
+    # save_player(player)
     # This check is only there to ensure that the correct parameter(i.e ab) is passed
     if algoType == 'ab':
         start = time.time()
         move = ab_bot(position, player)
         end = time.time()
-        # Reccods time by creating a new file
+        # Record time by creating a new file
         with open(ab_time_filename, 'w+') as timeFile_for_ab:
             timeFile_for_ab.write(str(end - start) + '\n')
     return move
